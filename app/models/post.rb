6069e56cf4c43cbaf1896d27289fc14bbcd2bf8b
Justin Searls <searls@gmail.com>
2018-01-23 09:31:13 -0500

    a buggy method that adds tags to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :author
   has_and_belongs_to_many :tags
+
+  def tag_post!(tag_name, author)
+    if existing_tag = Tag.find_by(:name => tag_name)
+      tags << existing_tag unless tags.include?(existing_tag)
+    else
+      tags << Tag.new(:name => tag_name, :author => self.author)
+    end
+  end
 end
